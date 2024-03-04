18fb94ae62bb16299f3363beb0774d4614a3f62e
Kethlen Martins <kethlen.silva@sou.inteli.edu.br>
2024-03-04 15:57:39 -0300

    código completo2

--- Projeto Individual Sem4/level1.js
+++ Projeto Individual Sem4/level1.js
@@ -7,31 +7,30 @@ class level1 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image ('background2', 'assets/backgroundLevel1.png');
-        this.load.spritesheet ('personagem', 'assets/spritesheetPersonagem.png', {startFrame:0, endFrame: 15, frameWidth:32, frameHeight:48});
-        this.load.image ('plataforma', 'assets/plataforma.png');
-        this.load.image ('chao', 'assets/chão.png')
-        this.load.image ('flor', 'assets/florPontuação.webp');
+        this.load.image ('background2', 'assets/backgroundLevel1.png'); // load do background
+        this.load.spritesheet ('personagem', 'assets/spritesheetPersonagem.png', {startFrame:0, endFrame: 15, frameWidth:32, frameHeight:48}); // load da personagem
+        this.load.image ('plataforma', 'assets/plataforma.png'); // load das plataformas
+        this.load.image ('chao', 'assets/chão.png') // load do chão
+        this.load.image ('flor', 'assets/florPontuação.webp'); // load da flor
     }
 
     create() {
 
-        this.pontuacao = 0
+        this.pontuacao = 0 // variável que guarda a pontuação
 
-        this.cameras.main.fadeIn(1000, 0, 0, 0); 
+        this.cameras.main.fadeIn(1000, 0, 0, 0); // inicialização com efeito de fade in
 
-        this.background = this.add.image (400, 300, 'background2');
+        this.background = this.add.image (400, 300, 'background2'); // cria o background
 
-        this.personagem = this.physics.add.sprite (0,0,'personagem').setScale(1.5);
+        this.personagem = this.physics.add.sprite (0,0,'personagem').setScale(1.5); // cria a personagem
+        this.personagem.setCollideWorldBounds(true); // define os limites de mundo para a personagem
 
-        this.personagem.setCollideWorldBounds(true);
+        this.chao = this.physics.add.staticImage (400, 600, 'chao'); // cria o chão
+        this.physics.add.collider(this.personagem, this.chao); // define as colisões entre a personagem e o chão
 
-        this.chao = this.physics.add.staticImage (400, 600, 'chao');
-        this.physics.add.collider(this.personagem, this.chao);
+        this.teclado = this.input.keyboard.createCursorKeys(); // permite o controle através das teclas
 
-        this.teclado = this.input.keyboard.createCursorKeys();
-
-        // Plataforma
+        // Plataformas
 
         this.plataforma = this.physics.add.staticImage(400, 200, 'plataforma'); 
         this.physics.add.collider(this.personagem, this.plataforma);
@@ -57,66 +56,67 @@ class level1 extends Phaser.Scene {
 
         // Flor        
         this.flor = this.physics.add.sprite(20, 50 ,'flor').setScale(0.025); // adiciona a flor na tela
-            this.flor.setCollideWorldBounds(true); // ativa os limites de mundo para a flor
-            this.flor.setBounce (0.4); // ativa o bounce da flor
-            this.physics.add.collider(this.flor, this.plataforma); // ativa a colisão entre a flor e as plataformas
+        this.flor.setCollideWorldBounds(true); // ativa os limites de mundo para a flor
+        this.flor.setBounce (0.4); // ativa o bounce da flor
+        this.physics.add.collider(this.flor, this.plataforma); // ativa a colisão entre a flor e as plataformas
 
         this.physics.add.overlap(this.personagem, this.flor, () => { // overlap entre a personagem e a flor
-            this.flor.setVisible (false); // a flor fica invisível
+        this.flor.setVisible (false); // a flor fica invisível
 
-            this.flor.y = Phaser.Math.RND.between (0,200); //posição vertical aleatória
-            this.flor.x = Phaser.Math.RND.between (50,650); //posição horizontal aleatória
+        this.flor.y = Phaser.Math.RND.between (0,200); //posição vertical aleatória
+        this.flor.x = Phaser.Math.RND.between (50,650); //posição horizontal aleatória
 
-            this.pontuacao += 1; // o número de pontos na variável pontuação crescerá em 1
+        this.pontuacao += 1; // o número de pontos na variável pontuação crescerá em 1
 
-            this.placar.setText ('Flores:' + this.pontuacao); // o texto do placar será atualizado com a nova quantidade de flores
+        this.placar.setText ('Flores:' + this.pontuacao); // o texto do placar será atualizado com a nova quantidade de flores
 
-            this.flor.setVisible (true); // a flor fica visível de novo
+        this.flor.setVisible (true); // a flor fica visível de novo
         });
 
-        this.anims.create ({
+        // animações de movimento do personagem
+        this.anims.create ({ // movimento para a direita
             key:'andarDireita',
             frames: this.anims.generateFrameNumbers('personagem', { start: 8, end: 11 }),
             frameRate: 8, 
-            repeat: -1,
+            repeat: -1, // repetição em loop
         })
 
-        this.anims.create ({
+        this.anims.create ({ // movimento para a esquerda
             key:'andarEsquerda',
             frames: this.anims.generateFrameNumbers('personagem', { start: 4, end: 7 }),
             frameRate: 8, 
-            repeat: -1,
+            repeat: -1, // repetição em loop
         })
 
     }
 
-    update() {    
+    update() { // controles com teclas de seta
         
-        if (this.teclado.left.isDown) {
-            this.personagem.anims.play ('andarEsquerda', true);
-            this.personagem.setVelocityX(-250); 
+        if (this.teclado.left.isDown) { // seta da esquerda pressionada
+            this.personagem.anims.play ('andarEsquerda', true); // display na animação de movimento para a esquerda
+            this.personagem.setVelocityX(-250); // movimento do personagem para a esquerda
     }
 
-        else if (this.teclado.right.isDown) { 
-            this.personagem.anims.play ('andarDireita', true);
-            this.personagem.setVelocityX(250);
+        else if (this.teclado.right.isDown) { // seta da direita pressionada
+            this.personagem.anims.play ('andarDireita', true); // display na animação de movimento para a direita
+            this.personagem.setVelocityX(250); // movimento do personagem para a direita
     }
 
-        else {
-            this.personagem.setFrame(0);
-            this.personagem.setVelocityX(0);
+        else { // nenhuma das teclas pressionadas
+            this.personagem.setFrame(0); // frame 0 será mostrado
+            this.personagem.setVelocityX(0); // a personagem não se move
     }
 
-        if (this.teclado.up.isDown && this.pulando == false) {
-        this.personagem.setVelocityY(-250);
-        this.pulando = true;
+        if (this.teclado.up.isDown && this.pulando == false) { // seta para cima pressionada
+            this.personagem.setVelocityY(-250); // movimento para cima
+            this.pulando = true;
     }
 
-        else { 
+        else { // a personagem cai pela ação da gravidade
     }
 
-    if (this.personagem.body.touching.down) {
-        this.pulando = false;
+        if (this.personagem.body.touching.down) { // função de controle do pulo quando a personagem está no chão
+            this.pulando = false;
     }
         
     }
--- Projeto Individual Sem4/welcomeScene.js
+++ Projeto Individual Sem4/welcomeScene.js
@@ -5,26 +5,26 @@ class welcomeScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image ('background1', 'assets/backgroundStart.png');
-        this.load.spritesheet ('botaoStart', 'assets/spritesheetBotaoStart.png', {startFrame: 0, endFrame: 1, frameWidth: 201, frameHeight: 59});
+        this.load.image ('background1', 'assets/backgroundStart.png'); //load do background
+        this.load.spritesheet ('botaoStart', 'assets/spritesheetBotaoStart.png', {startFrame: 0, endFrame: 1, frameWidth: 201, frameHeight: 59}); // load do botão
     }
 
     create() {
-        this.background = this.add.image (400, 300, 'background1');
-        this.botao = this.add.sprite (400, 427, 'botaoStart');
-        this.botao.setInteractive();
+        this.background = this.add.image (400, 300, 'background1'); // cria o background
+        this.botao = this.add.sprite (400, 427, 'botaoStart'); // cria o botão
+        this.botao.setInteractive(); // define o botão como um elemento interativo
 
-        this.botao.on('pointerover', () => {
-          this.botao.setFrame(1);
+        this.botao.on('pointerover', () => { // ao passar o mouse sobre o botão
+          this.botao.setFrame(1); // o frame 1 é mostrado
         });
-        this.botao.on('pointerout', () => {
-            this.botao.setFrame(0);
+        this.botao.on('pointerout', () => { // ao tirar o mouse do botão
+            this.botao.setFrame(0); // o frame 0 é mostrado
         })
-        this.botao.on('pointerdown', this.mudarCena, this)
+        this.botao.on('pointerdown', this.mudarCena, this) // ao clicar no botão, o método mudarCena é chamado
 
     }
 
-    mudarCena () {
+    mudarCena () { // inicia a cena do level1
         this.scene.start('level1');
     }
 
